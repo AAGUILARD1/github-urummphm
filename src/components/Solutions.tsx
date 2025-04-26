3bd498109bc1471b0bfaac36ada40e5e39c2e0eb
AAGUILARD1 <institucion74@gmail.com>
2025-04-25 21:54:11 -0600

    Update Solutions.tsx

--- src/components/Solutions.tsx
+++ src/components/Solutions.tsx
@@ -8,7 +8,7 @@ import iconoMarketing from '../assets/icono-marketing.jpg';
 const services = [
   {
     title: 'Inteligencia estratégica para optimizar tus decisiones y resultados. La eficiencia de tu empresa, elevada por IA.',
-    video: '/video-servicio-1.mp4',
+    video: '/decisiones.mp4',
     icon: iconoCRM,
   },
   {
